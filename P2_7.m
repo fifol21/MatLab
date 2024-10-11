d2553af57d2eced84d40f1ceba62a7732a2e8514
fifol21 <166660857+fifol21@users.noreply.github.com>
2024-10-11 17:52:10 +0200

    Add files via upload

---
+++ P2_7.m
@@ -0,0 +1,41 @@
+clear all; 
+close all;
+clear all;
+close all;
+
+
+single_min = realmin("single"),
+single_max = realmax("single"),
+single_eps = eps("single"), % blad jaki mozeny uzyskac w obliczeniach 
+
+double_max = realmin("double"),
+double_min = realmax("double"),
+double_eps = eps("double"),
+
+
+int8_min = intmin("int8"),
+int8_max = intmax("int8"),
+
+uint8_min = intmin("uint8"), %unsigned int czyli tylko liczby calkowite 0:255
+uint8_max = intmax("uint8"),
+
+
+int32_min = intmin("int32"),
+int32_max = intmax("int32"),
+
+uint32_min = intmin("uint32"), 
+uint32_max = intmax("uint32"),
+
+int64_min = intmin("int64"),
+int64_max = intmax("int64"),
+
+uint64_min = intmin("uint64"),
+uint64_max = intmax("uint64"),
+
+
+
+
+
+
+
+
