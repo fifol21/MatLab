9768ffae1fc8219b2285de18899791435b08404e
fifol21 <166660857+fifol21@users.noreply.github.com>
2024-10-11 18:47:35 +0200

    Add files via upload

---
+++ P2_13.m
@@ -0,0 +1,22 @@
+clear all;
+close all;
+
+a= 3;
+b = 2985454;
+c = 1;
+
+delta = sqrt(b^2 -4*a*c),
+x_1 = (-b-delta)/(2*a),
+x_2 = (-b+delta)/(2*a),
+
+if ((abs(x_1)) > (abs(x_2)))
+    X1 = x_1;
+else
+    X1= x_2;
+end
+
+    X2 = c/(a*X1),
+    X1,
+
+
+
